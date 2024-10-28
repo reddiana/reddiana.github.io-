02f5dce2b8c237ebafcfb97499d8caabf9948640
Seil Suh <reddiana@gmail.com>
2024-10-28 20:49:58 +0900

    Quartz sync: Oct 28, 2024, 8:49 PM

--- runQuartz.bat
+++ runQuartz.bat
@@ -1,2 +1,6 @@
+cd /d "%~dp0"
 rmdir /S /Q public
+
+explorer http://localhost:8080
+
 npx quartz build --serve
--- syncQuartz.bat
+++ syncQuartz.bat
@@ -1,4 +1,5 @@
 explorer https://github.com/reddiana/reddiana.github.io/actions
 
+cd /d "%~dp0"
 npx quartz sync
 
